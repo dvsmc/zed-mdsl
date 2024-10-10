786ca3b4fdaf30186b3d972895d0b31aada4add9
dvsmc <112554734+dvsmc@users.noreply.github.com>
2024-10-10 14:57:41 +0300

    Better highlights

--- languages/mdsl/highlights.scm
+++ languages/mdsl/highlights.scm
@@ -26,7 +26,14 @@
     (root_node) @keyword
     (composite_node) @keyword
     (decorator_node) @keyword
-    (leaf_node) @keyword
-
+    (leaf_node) @function
+    (callbacks) @function.builtin
+    (guards) @conditional
     (comment) @comment
+    (identifier) @variable
+    (node_args) @parameter
+    (number) @number
+    (string) @string
+    (boolean) @constant.builtin
+    "null" @constant.builtin
 ]
