3caf345665892b5899ee7a2c9abd63884384b656
dvsmc <112554734+dvsmc@users.noreply.github.com>
2024-10-10 14:49:01 +0300

    Something working

---
+++ languages/mdsl/highlights.scm
@@ -0,0 +1,32 @@
+; Highlight node types, actions, conditions, and comments
+; [
+;   (root) @keyword
+;   (sequence) @keyword
+;   (selector) @keyword
+;   (parallel) @keyword
+;   (lotto) @keyword
+;   (repeat) @keyword
+;   (retry) @keyword
+;   (action) @function
+;   (condition) @function
+;   (wait) @number
+;   (branch) @function
+;   (entry) @function
+;   (exit) @function
+;   (step) @function
+;   (while) @conditional
+;   (until) @conditional
+
+;   (comment) @comment
+;   (/*) @comment
+;   (*/) @comment
+; ]
+
+[
+    (root_node) @keyword
+    (composite_node) @keyword
+    (decorator_node) @keyword
+    (leaf_node) @keyword
+
+    (comment) @comment
+]
